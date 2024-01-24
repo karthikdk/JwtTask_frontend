757da32f49233865dfecd95dccf8111e052993c0
karthik <dk.karthikd9@gmail.com>
2024-01-24 15:15:16 +0530

    frontend:registration login and navbar setup created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Registration'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1>JWT Token</h1>
+    <div className='container-fluid'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+      </Routes>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import {Navbar, Nav} from 'react-bootstrap'
 
 const NavBar = () => {
-  return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
-  )
+    const navigate = useNavigate()
+    
+    const handleClick = () => {
+        localStorage.removeItem('loginToken')
+        navigate("/login")
+    }
+
+    return (
+        <div>
+            <Navbar bg="primary" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+                <Navbar.Brand href="/">JWT</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav variant="pills" defaultActiveKey="/" className="ms-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                        {localStorage.getItem('loginToken') ? <Nav.Link onClick={handleClick}>Logout</Nav.Link> : <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+                    </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        </div>
+    
+    )
 }
 
 export default NavBar
